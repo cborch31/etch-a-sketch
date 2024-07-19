fef919dc049e5d18b26e87da35272d8f6b7e23b8
Carolyn Borcherding <166224066+cborch31@users.noreply.github.com>
2024-07-19 18:29:08 -0400

    update grid layout using flexBasis

--- javascript.js
+++ javascript.js
@@ -2,11 +2,17 @@
 //create grid of divs using loop
 const gridContainer = document.querySelector('#gridContainer'); //remember: # refers to an ID tag
 
-for (let i=0; i<16; i++){
-    for (let j=0; j<16; j++) {
+let gridAmount = 16; //will update to include user selection (cannot be greater than 100)
+
+for (let i=0; i<gridAmount; i++){
+    for (let j=0; j<gridAmount; j++) {
         const cell = document.createElement('div');
         cell.classList.add('cell');
         gridContainer.appendChild(cell);
     } 
 };
 
+const cellList = document.querySelectorAll('cell');
+// cellList.style.flexBasis = '6.25%'; this doesn't work.
+
+console.log(cellList); //returns a nodeList
